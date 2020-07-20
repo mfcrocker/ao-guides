2f53ff54c86d4520f939cad4a42aa42e99d01769
Matt Crocker <matt.crocker@gmail.com>
2020-07-20 22:11:56 +0100

    A test test

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-const server = app.listen(7000, () => {
-    console.log(`Express running -> PORT ${server.address().port}`)
-})
---
+++ tests/test-app.js
@@ -0,0 +1,9 @@
+const expect  = require('chai').expect;
+const request = require('request');
+
+it('Main page content', function(done) {
+    request('http://localhost:8080' , function(error, response, body) {
+        expect(body).to.equal('Hello World');
+        done();
+    });
+});
